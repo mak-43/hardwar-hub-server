a73983fe390a0a1c74e1d2455902621d8181eca2
Mak-43 <letsmailmak@gmail.com>
2022-07-11 00:57:15 +0600

    commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const jwt=require('jsonwebtoken')
+const app=express()
+const port=process.env.PORT||5000 
+const { MongoClient,ServerApiVersion, ObjectId } = require('mongodb');
+
+
+app.use(cors()) 
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bxqusfm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+       await client.connect()
+
+       const productCollection = client.db('Assignment-12').collection('tools')
+       const orderCollection = client.db('Assignment-12').collection('orders')
+       const updateproCollection = client.db('Assignment-12').collection('updatepro')
+       const userCollection = client.db('Assignment-12').collection('user')
+
+        app.get('/tools',async(req,res)=>{
+            const tools= await productCollection.find().toArray()
+            res.send(tools)
+        })
+
+        app.get('/tools/:id',async(req,res)=>{
+            const id=req.params.id 
+            const query={_id:ObjectId(id)}
+            const product=await productCollection.findOne(query) 
+            res.send(product)
+        })
+
+        app.post('/order',async(req,res)=>{
+            const order=req.body 
+            const result=await orderCollection.insertOne(order)
+            res.send(result)
+        })
+        
+        app.get('/order',async(req,res)=>{
+            const email=req.query.email
+            const query={email:email}
+            const pro= await orderCollection.find(query).toArray()
+            res.send(pro)
+        })
+
+        app.get('/payment/:id',async(req,res)=>{
+            const id=req.params.id 
+            const query={_id:ObjectId(id)} 
+            const result= await orderCollection.findOne(query) 
+            res.send(result)
+        })
+
+        app.put('/updatepro',async(req,res)=>{  
+            const update=req.body
+            // console.log(update)
+            const filter={email:update.email}
+            const options={upsert:true}
+            const updateDoc={
+                    $set:update
+            }
+            const result=await updateproCollection.updateOne(filter,updateDoc,options)
+            res.send(result)
+        })
+
+        app.get('/userpro',async(req,res)=>{
+            const email=req.query.email
+            const query={email:email}
+            const pro= await updateproCollection.find(query).toArray()
+            res.send(pro)
+        })
+      
+
+        app.put('/user/:email',async(req,res)=>{
+            const email=req.params.email 
+            const user=req.body 
+            const filter={email:email}
+            const options={upsert:true}
+            const updateDoc={
+                $set:user,
+            }
+            const result=await userCollection.updateOne(filter,updateDoc,options)
+            const token=jwt.sign({email:email},process.env.ACCESS_TOKEN,{expiresIn:'1d'})
+            res.send({result,token})
+        })
+       
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
+
+app.get('/',(req,res)=>{
+    res.send('Running Server !!!') 
+})
+app.listen(port,()=>{
+    console.log('Listening to port',port)
+})
