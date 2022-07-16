7d4bc4af59ff22a575379c9b27a62666c594d51b
Mak-43 <letsmailmak@gmail.com>
2022-07-16 22:28:33 +0600

    'l'

--- index.js
+++ index.js
@@ -231,6 +231,13 @@ async function run() {
             const users = await userCollection.find().toArray()
             res.send(users)
         })
+        //delete user 
+        app.delete('/duser/:id',async(req,res)=>{
+            const id=req.params.id 
+            const query={_id:ObjectId(id)}
+            const result=await userCollection.deleteOne(query)
+            res.send(result)
+        })
         //user save and update
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email
