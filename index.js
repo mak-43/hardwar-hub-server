ae9731c7f38855b5f627dc117f6790d2caa39177
Mak-43 <letsmailmak@gmail.com>
2022-07-16 23:41:40 +0600

    'cors'

--- index.js
+++ index.js
@@ -11,17 +11,10 @@ const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(express.json())
 app.use(cors({
-    origin: function(origin, callback){
-      // allow requests with no origin 
-      // (like mobile apps or curl requests)
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
   }));
 
 
